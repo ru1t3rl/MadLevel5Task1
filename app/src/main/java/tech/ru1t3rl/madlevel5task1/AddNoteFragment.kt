8cffbadda9694928b6ed0d8362535cc6a0819ade
ru1t3rl <luca.ruiters@hva.nl>
2020-10-19 11:35:20 +0100

    Linked the the viewModel to the fragments

--- app/src/main/java/tech/ru1t3rl/madlevel5task1/AddNoteFragment.kt
+++ app/src/main/java/tech/ru1t3rl/madlevel5task1/AddNoteFragment.kt
@@ -6,11 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_note.*
 import tech.ru1t3rl.madlevel5task1.databinding.FragmentAddNoteBinding
 
 class AddNoteFragment : Fragment() {
     private lateinit var binding: FragmentAddNoteBinding
+
+    private val viewModel: NoteViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +31,37 @@ class AddNoteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnSave.setOnClickListener {
+            saveNote()
             findNavController().navigate(R.id.action_addNoteFragment_to_notepadFragment)
         }
+        observeNote()
     }
+
+    private fun observeNote() {
+//fill the text fields with the current text and title from the viewmodel
+        viewModel.note.observe(viewLifecycleOwner, Observer { note ->
+            note?.let {
+                etTitle?.setText(it.titleText)
+                etNote?.setText(it.text)
+            }
+
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, Observer { message ->
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.success.observe(viewLifecycleOwner, Observer { success ->
+            //"pop" the backstack, this means we destroy this    fragment and go back to the RemindersFragment
+            findNavController().popBackStack()
+        })
+    }
+
+    private fun saveNote() {
+        viewModel.updateNote(
+            etTitle?.text.toString(),
+            etNote?.text.toString()
+        )
+    }
+
 }
--- app/src/main/java/tech/ru1t3rl/madlevel5task1/NoteViewModel.kt
+++ app/src/main/java/tech/ru1t3rl/madlevel5task1/NoteViewModel.kt
@@ -1,12 +1,48 @@
 package tech.ru1t3rl.madlevel5task1
 
 import android.app.Application
+import android.content.res.Resources
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.*
 
-class NoteViewModel(application: Application):
-    AndroidViewModel(application) {
+class NoteViewModel(application: Application): AndroidViewModel(application) {
     private val noteRepository = NoteRepository(application.applicationContext)
+    private val mainScope = CoroutineScope(Dispatchers.Main)
 
     val note = noteRepository.getNotepad()
+    val error = MutableLiveData<String>()
+    val success = MutableLiveData<Boolean>()
+
+    fun updateNote(title: String, text: String) {
+        val newNote =  Note(
+            id = note.value?.id,
+            titleText = title,
+            lastUpdated = Date(),
+            text = text
+        )
+
+        if(isNoteValid(newNote)){
+            mainScope.launch {
+                withContext(Dispatchers.IO){
+                    noteRepository.updateNotepad(newNote)
+                }
+                success.value = true
+            }
+        }
+    }
+
+    private fun isNoteValid(note: Note): Boolean {
+        return when{
+            note.titleText.isBlank() -> {
+                error.value = Resources.getSystem().getString(R.string.empty_title)
+                    false
+            }
+            else -> return true
+        }
+    }
 }
